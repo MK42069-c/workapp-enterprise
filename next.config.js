ea94765f92f7330d8be7ee46f6bac5ef5d853ed4
MK42069-c <gsplaybook1@gmail.com>
2025-11-05 23:10:16 +0200

    Add Next.js PWA configuration with enterprise features

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  sw: 'service-worker.js',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
